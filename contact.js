eaf3bab5e739dc9aed8d82d4f4966e941ae9110d
nishu kumari <nishukumari776605@gmail.com>
2026-09-15 19:28:49 +0530

    campus connect website

---
+++ contact.js
@@ -0,0 +1,28 @@
+function sendMessage() {
+    let name = document.getElementById("name").value;
+
+    if (name == "") {
+        alert("Please enter your name");
+    } else {
+        document.getElementById("result").innerHTML =
+        "Thank you, " + name + "!";
+    }
+}
+
+function sendMessage() {
+
+    let name = document.getElementById("name").value;
+
+    if (name == "") {
+
+        alert("Please enter your name");
+
+    } else {
+
+        document.getElementById("result").innerHTML =
+
+        "Thank you, " + name + "!";
+
+    }
+
+}
---
+++ script.js
@@ -0,0 +1,4 @@
+function submitForm(event) {
+    event.preventDefault();
+    alert("Your enquiry has been submitted!");
+}
